971b0e5dd66ade43032202de11561953fd726199
akshaya78010 <147473029+akshaya78010@users.noreply.github.com>
2024-10-28 23:30:02 +0530

    Add files via upload

---
+++ WrapperClass.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+/*Wrapper classes:
+------------------
+->Provides an object representation for primitive values.
+->some classes in java are written in such a way that they only accept objects and not primitive values.
+->to use primitive data tye values with those classes
+->Wrapper classes provide object representation of every primitive dqta type we have in java .
+Primitive Data type                  Corresponding Wrapper class
+-------------------------------------------------------------------
+int                                    Integer
+float                                  Float
+char                                   Character
+double                                 Double
+short                                  Short
+long                                   Long
+byte                                   Byte
+Boolean                                Boolean
+*/
+/* 
+public class WrapperClasses{
+    public static void main(String args[])
+    {
+        ArrayList<Integer>arr1 = new ArrayList<>();
+        arr1.add(10);
+        arr.add(20);
+        for(Integer i:arr1)
+        {
+            System.out.print(i + " ");
+        }
+        ArrayList<item>arr = new ArrayList<>();
+        arr.add(new Item("laptop",7));
+        arr.add(new Item("mouse",6));
+        for(Item i:arr){
+            System.out.println(i + " ");
+        }
+    }
+}*/
+
